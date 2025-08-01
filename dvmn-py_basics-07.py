b2305e4fac45a60b404fcf5268b11abbd3fb7c34
kurtsev-artem <66749796+kurtsev-artem@users.noreply.github.com>
2025-08-01 12:15:28 +0300

    Add files via upload

--- dvmn-py_basics-07.py
+++ dvmn-py_basics-07.py
@@ -11,7 +11,7 @@ bot = ptbot.Bot(TG_TOKEN)
 timeout = 5
 
 
-def answer_timeout(chat_id, question):
+def answer_timeout(chat_id):
     bot.send_message(chat_id, "Время вышло")
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -22,10 +22,10 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)    
 
-def reply(chat_id, question):
+def reply(chat_id,question):
     message_id =  bot.send_message(chat_id, "Запускаю таймер...")
-    bot.create_countdown(timeout, notify, chat_id=chat_id,message_id = message_id)
-    bot.create_timer(timeout, answer_timeout, chat_id=chat_id, question=question)
+    bot.create_countdown(timeout, notify, chat_id=chat_id,message_id=message_id)
+    bot.create_timer(timeout, answer_timeout, chat_id=chat_id)
 
 def notify(secs_left, chat_id, message_id):
     bot.update_message(chat_id,message_id ,"Осталось {} секунд!\n {}".format(secs_left,render_progressbar(timeout, timeout - secs_left))  )
