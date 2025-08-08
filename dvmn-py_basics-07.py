95a5b5af7fb5ef06831a79f2975960ca1d4c14b5
kurtsev-artem <66749796+kurtsev-artem@users.noreply.github.com>
2025-08-08 13:14:05 +0300

    Add files via upload

--- dvmn-py_basics-07.py
+++ dvmn-py_basics-07.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 TIMEOUT = 5
 
 
-def answer_timeout(chat_id):
+def answer_timeout(chat_id,bot):
     bot.send_message(chat_id, "Время вышло")
     
 
@@ -18,21 +18,21 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def reply(chat_id,question):
+def reply(chat_id,question,bot):
     message_id =  bot.send_message(chat_id, "Запускаю таймер...")
-    bot.create_countdown(TIMEOUT, notify, chat_id=chat_id,message_id=message_id)
-    bot.create_timer(TIMEOUT, answer_timeout, chat_id=chat_id)
+    bot.create_countdown(TIMEOUT, notify, chat_id=chat_id,message_id=message_id,bot=bot)
+    bot.create_timer(TIMEOUT, answer_timeout, chat_id=chat_id,bot=bot)
     
 
-def notify(secs_left, chat_id, message_id):
+def notify(secs_left, chat_id, message_id,bot):
     bot.update_message(chat_id,message_id ,"Осталось {} секунд!\n {}".format(secs_left,render_progressbar(TIMEOUT, TIMEOUT - secs_left))  )
     
 
 def main():
     load_dotenv()
-    TG_TOKEN = os.getenv('TG_TOKEN')
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(reply)  
+    tg_token = os.getenv('TG_TOKEN')
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(reply,bot=bot)  
     bot.run_bot()
     
 
